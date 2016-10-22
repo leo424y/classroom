00e0ef606f7f50510a439156699090833fdbc46f
leo424y <leo424y@gmail.com>
2016-10-22 16:07:54 +0800

    controller test: PROTOCAL +

--- spec/controllers/course_controller_spec.rb
+++ spec/controllers/course_controller_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
-RSpec.describe CourseController, type: :controller do
+RSpec.describe CoursesController, type: :controller do
+  # 期待 index action ，要撈出所有站上所有的 courses，assigns 到 @courses 並顯示出來。
+  # https://www.relishapp.com/rspec/rspec-rails/docs/matchers/render-template-matcher
+  # GET index
+  # POST create
+  # PUT update
+  # DELETE destroy
+  describe 'GET index' do
+    it 'assigns @courses and render template' do
+      course1 = Course.create(title: 'foo', description: 'bar')
+      course2 = Course.create(title: 'bar', description: 'foo')
 
+      get :index
+
+      expect(assigns[:courses]).to eq([course1, course2])
+      expect(response).to render_template('index')
+    end
+  end
 end
