a197851a75c953dcdefafaaf352005792f25cde0
leo424y <leo424y@gmail.com>
2016-10-22 17:02:56 +0800

    F NameError

--- spec/controllers/course_controller_spec.rb
+++ spec/controllers/course_controller_spec.rb
@@ -1,12 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe CoursesController, type: :controller do
-  # 期待 index action ，要撈出所有站上所有的 courses，assigns 到 @courses 並顯示出來。
-  # https://www.relishapp.com/rspec/rspec-rails/docs/matchers/render-template-matcher
-  # GET index
-  # POST create
-  # PUT update
-  # DELETE destroy
+RSpec.describe CourseController, type: :controller do
   describe 'GET index' do
     it 'assigns @courses and render template' do
       course1 = Course.create(title: 'foo', description: 'bar')
