aa86c56067e53126cdae2688a620f78b2c47bf10
leo424y <leo424y@gmail.com>
2016-10-23 09:38:04 +0800

    FactoryGirl.create(:?)

--- spec/controllers/courses_controller_spec.rb
+++ spec/controllers/courses_controller_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 RSpec.describe CoursesController, type: :controller do
   describe 'GET index' do
     it 'assigns @courses and render template' do
-      course1 = Course.create(title: 'foo', description: 'bar')
-      course2 = Course.create(title: 'bar', description: 'foo')
+      course1 = FactoryGirl.create(:course)
+      course2 = FactoryGirl.create(:course)
 
       get :index
 
