1f01dc3a71ec32fbff50e5e0aa58566a81603398
leo424y <leo424y@gmail.com>
2016-10-24 23:21:02 +0800

    4.1.2.11 :optional If you set the :optional option to true, then the presence of the associated object won't be validated. By default, this option is set to false.

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,4 +1,6 @@
 class Course < ApplicationRecord
-  belongs_to :user
+  # http://stackoverflow.com/questions/38983666/validation-failed-class-must-exist
+  # 4.1.2.11 :optional If you set the :optional option to true, then the presence of the associated object won't be validated. By default, this option is set to false.
+  belongs_to :user, optional: true
   validates :title, presence: true
 end
